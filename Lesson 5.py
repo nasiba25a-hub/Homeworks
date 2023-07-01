2d29c10182389b6a2ec22366fa3780455363b926
nasiba25a-hub <135620153+nasiba25a-hub@users.noreply.github.com>
2023-07-01 15:05:13 +0500

    Lesson 5.py

---
+++ Lesson 5.py
@@ -0,0 +1,80 @@
+# #for1
+# k=int(input("k uchun son kiriting:"))
+# n=int(input("n uchun son kiriting:"))
+# for x in range(0,n):
+#     print(x, k )
+#
+# #for2
+# b=int(input("b uchun son kiriting:"))
+# a=int(input("a uchun son kiriting:(NOTE! a<b)"))
+# for x in range(a+1,b):
+#     print(x)
+#
+# #for3
+# a=int(input("a uchun son kiriting:"))
+# b=int(input("b uchun son kiriting:(NOTE! a<b)"))
+# for x in range(b-1,a,-1):
+#     print(x)
+#
+# #for4
+# n=int(input("Narxini kiriting:"))
+# kg=int(input("Kilosini kiriting:"))
+# for x in range(1,kg):
+#     print(x*n)
+#
+# #for5
+# n=int(input("Narxini kiriting:"))
+# for x in range(1,11):
+#     print(x/10*n)
+#
+# #for6
+# n=int(input("Narxini kiriting:"))
+# for x in range(1,11):
+#     print((x/10+1)*n)
+#
+# #for7
+# a = int(input('a uchun son kiriting: '))
+# b = int(input('b uchun son kiriting: '))
+# total = 0
+# for x in range(a,b+1):
+#     total+=x
+# print('Yigindi:',total)
+#
+# #for8
+# a = int(input('a uchun son kiriting: '))
+# b = int(input('b uchun son kiriting: '))
+# total = 1
+# for x in range(a,b+1):
+#     total*=x
+# print('Result:',total)
+#
+# #for9
+# a = int(input('a uchun son kiriting: '))
+# b = int(input('b uchun son kiriting: '))
+# total = 0
+# for x in range(a,b+1):
+#     kvadrat=x**2
+#     total+=kvadrat
+# print('Result:',total)
+#
+# # for 10
+# n = int(input('Son kiriting: '))
+# total = 0
+# for x in range(1,n+1):
+#     total+=1/x
+# print(round(total,2))
+#
+# # for 11
+# n = int(input('Son kiriting: '))
+# total = 0
+# for x in range(n, 2*n+1):
+#     total += x**2
+# print("Result:", total)
+#
+# # for 12
+# n = int(input('Son kiriting: '))
+# total = 1
+# for x in range(11,(n*10)+1):
+#     c = x/10
+#     total*=c
+# print('Result: ',round(total,2))
